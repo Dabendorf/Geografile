e2dcbc71294dbca027b0e33e4a1a40adae685183
Lukas Schramm <lukas@dorpapst.de>
2023-11-18 13:17:01 +0100

    Init

---
+++ Geografile/init.sh
@@ -0,0 +1,9 @@
+git@github.com:Dabendorf/Bergle.git
+git@github.com:Dabendorf/Kommundle.git
+git@github.com:Dabendorf/Tradle.git
+git@github.com:Dabendorf/Worldle.git
+
+mv Bergle ../bergle.privatevoid.eu
+mv Worldle ../worldle.privatevoid.eu
+mv Tradle ../tradle.privatevoid.eu
+mv Kommundle ../kommundle.privatevoid.eu
